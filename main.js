640a476dfbfc8af9e9663d4526a97c7028219595
Jason Wang <jasonxwang9@gmail.com>
2023-05-05 22:49:04 -0400

    popup modal cards

---
+++ main.js
@@ -0,0 +1,27 @@
+for (let i = 0; i < 12; i++) {
+    try {
+        // Get modal button and modal
+        const modalButton = document.getElementById('modal-button' + i);
+        const modal = document.getElementById('my-modal' + i);
+
+        // Get close button and modal background
+        const closeButton = document.getElementById('modal-close' + i);
+        const modalBackground = document.getElementById('modal-background');
+
+        // When the user clicks the modal button, open the modal
+        modalButton.addEventListener('click', () => {
+            modal.classList.add('is-active');
+        });
+
+        // When the user clicks the close button or modal background, close the modal
+        closeButton.addEventListener('click', () => {
+            modal.classList.remove('is-active');
+        });
+
+        modalBackground.addEventListener('click', () => {
+            modal.classList.remove('is-active');
+        });
+    }catch(e) {}
+
+}
+
